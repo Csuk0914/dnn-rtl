46d633441bc0df0323ec9097757d8f4b824940f4
souryadey <souryade@usc.edu>
2017-01-24 00:35:26 -0800

    improved gaussian number generator

--- scripts/gaussian_number_generator.py
+++ scripts/gaussian_number_generator.py
@@ -1,13 +1,17 @@
 import numpy as np
 import os
 
-def gaussian_list_generate(fi, fo, int_bits, frac_bits, filename='f'):
+def gaussian_list_generate(fi, fo, int_bits, frac_bits, filename=os.path.dirname(os.path.realpath(__file__)) + '/s136_frc21_int10.dat'):
+    ''' Remember to move the created file to dnn-rtl/gaussian_list'''    
+    f = open(filename,'wb')    
     width = frac_bits + int_bits + 1 #1 for sign bit
     x = np.random.normal(0,np.sqrt(2./(fi+fo)),2000)
     x[x>2**int_bits-2**(-frac_bits)] = 2**int_bits-2**(-frac_bits) #positive limit
     x[x<-2**int_bits] = -2**int_bits #negative limit
     for i in xrange(len(x)):
         x_bin = format(int(2**(width+1) + x[i]*(2**frac_bits)), 'b')
-        print (x_bin[-width:])
+        f.write('{0}\n'.format(x_bin[-width:]))
+    f.close()
 
-gaussian_list_generate(128,8,10,21)
+gaussian_list_generate(128,8,10,21, filename=os.path.dirname(os.path.realpath(__file__)) + '/s136_frc21_int10.dat')
+gaussian_list_generate(32,8,10,21, filename=os.path.dirname(os.path.realpath(__file__)) + '/s40_frc21_int10.dat')
